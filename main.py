096e1abd00833a0950aac2530a333d57845058d5
mathdantas <48970233+mathdantas@users.noreply.github.com>
2022-01-10 18:14:44 -0300

    feat(ToDo): Criando funcao de adicionar tarefa ao arquivo csv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import inquirer
+import csv
+
 def menu():
 
     lista_menu = [
@@ -16,3 +18,28 @@ def menu():
     ]
 
     return inquirer.prompt(lista_menu)
+# o título, a data de realização e a categoria da tarefa. Você deverá salvar essas três informações 
+# (além de uma informação de que o status da tarefa está como Pendente) dentro de um arquivo CSV 
+# (tarefas.csv, por exemplo).
+def adicionar_tarefa():
+    lista_tarefa = [
+        inquirer.Text("titulo", message="Titulo: "),
+        inquirer.Text("data_de_realizacao", message="Data de realização: "),
+        inquirer.List(
+        "categoria",
+            message="O que você deseja fazer?",
+            choices=[
+                "Pessoal",
+                "Profissional"
+                ],
+        )
+    ]
+    tarefa = inquirer.prompt(lista_tarefa)
+    tarefa['status'] = 'Pendente'
+    tarefa_lista = list(tarefa.values())
+    
+    with open('tarefas.csv', 'a') as tarefas_csv:
+        escritor = csv.writer(tarefas_csv, delimiter=';', lineterminator='\n')
+        escritor.writerow(tarefa_lista)
+
+adicionar_tarefa()
