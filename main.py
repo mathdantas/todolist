a25207e4c26976dbac507f317d5ba7b7fefc87ba
VicDaArSh <daisukearaki@usp.br>
2022-01-10 16:56:12 -0300

    '

---
+++ main.py
@@ -0,0 +1,21 @@
+import inquirer
+def menu():
+
+    lista_menu = [
+        inquirer.List(
+        "escolha_menu",
+            message="O que você deseja fazer?",
+            choices=[
+                'Adicionar tarefa',
+                'Alterar status da tarefa',
+                'Remover tarefa',
+                'Tarefas de outro dia',
+                'Encerrar'
+                ],
+        ),
+    ]
+
+    return inquirer.prompt(lista_menu)
+
+
+print(menu())
